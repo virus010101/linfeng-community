bb104107ed14453fbd0113b47f5637dcfbb02d68
linfeng <2445465217@qq.com>
2023-09-06 11:20:46 +0800

    新增uniapp端404模块页面

---
+++ src/main/resources/static/linfeng-community-uniapp-ky/README.md
@@ -0,0 +1,28 @@
+### 一、启动方式
+
+林风社交论坛用户端linfeng-community-uniapp-ky启动方式：
+
+将项目导入HBuilderX编辑器，初次使用请在项目根目录执行npm install安装依赖，
+一定要执行npm install安装uview依赖，才能运行。
+运行到H5端请执行`运行`—>`运行到浏览器`
+运行到微信小程序需要注意，先在`mainfest.json`—>`微信小程序配置`，设置小程序APPID，
+然后再`运行`—>`运行到小程序模拟器`—>`微信开发者工具`
+启动后台前端vue项目，在后台管理系统配置小程序AppID和密钥才能在uniapp用户端实现微信登录。
+在后台管理系统还需要配置云存储信息，才能在uniapp用户端实现图片的上传。
+注意：开源版适配H5和微信小程序端。
+
+
+
+### 二、简介
+
+**标准版和开源版的区别**：
+
+https://net.linfeng.tech/version/version.html
+
+开源版运行效果：
+
+ [查看开源版移动端效果图](https://pic.linfeng.tech/test/20230724/7bace9859cef4e2ca4a23a4a687a248e.gif)
+
+了解更多请查看：
+
+[林风社交论坛官网](https://net.linfeng.tech) 
---
+++ src/main/resources/static/linfeng-community-uniapp-ky/pages/error/error.vue
@@ -0,0 +1,70 @@
+<template>
+	<view class="error">
+		<text class="title">页面迷路啦~</text>
+		<text class="info">请确认访问地址是否有误</text>
+		<u-button type="primary" class="btn-home" @click="goHome">返回首页</u-button>
+		<u-button type="success" @click="goWebsite">前往官网</u-button>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			goHome(){
+				uni.switchTab({
+					url:'/pages/index/index'
+				})
+			},
+			goWebsite(){
+				// #ifdef H5
+				window.open("https://net.linfeng.tech")
+				// #endif
+				// #ifdef MP-WEIXIN
+				uni.navigateToMiniProgram({
+					appId: 'wx1d4a8967c92bda9b',
+					path: 'pages/index/index',
+					success: res => {
+						console.log("打开成功", res);
+					},
+					fail: err => {
+						console.log(err);
+					}
+				})
+				// #endif
+				
+			}
+			
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.error {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		height: 80vh;
+
+		.title {
+			margin-bottom: 100rpx;
+			color: #aaaaff;
+			font-weight: 600;
+			font-size: 60rpx;
+		}
+
+		.info {
+			margin-bottom: 20rpx;
+			color: #333;
+			font-weight: 500;
+			font-size: 40rpx;
+		}
+
+		.btn-home {
+			margin-bottom: 40rpx;
+		}
+	}
+</style>
