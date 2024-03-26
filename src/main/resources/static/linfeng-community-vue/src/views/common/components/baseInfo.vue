753776dd30c091af14695832bc80599f44e415ca
linfeng <2445465217@qq.com>
2024-03-26 11:38:33 +0800

    优化后台管理系统前端首页页面

--- src/main/resources/static/linfeng-community-vue/src/views/common/components/baseInfo.vue
+++ src/main/resources/static/linfeng-community-vue/src/views/common/components/baseInfo.vue
@@ -80,6 +80,97 @@
         </el-card>
       </el-col>
     </el-row>
+        <el-row :gutter="20">
+      <el-col :sm="24" :lg="12" style="padding-left: 20px">
+        <h2>林风社交论坛系统开源版</h2>
+        <p>
+          林风社交论坛uniapp小程序/H5/APP版本是前后端分离的社交论坛问答发帖/BBS/SNS项目。 
+          项目分为Uniapp用户端（兼容H5、微信小程序）和Vue2后台管理端 包括完整的权限处理。
+        </p>
+        <h2>林风社交论坛系统标准商业版</h2>
+        <p>
+          林风社交论坛标准商业版包括（兼容H5、微信小程序、APP端、微信公众号端）和Vue3后台管理端。
+          包括：图文帖，长文贴，短视频，私密圈子，公开圈子，私聊，微信支付（小程序/H5/app/公众号），付费贴，积分签到，钱包充值，积分余额兑换，话题标签，抽奖大转盘，手机号邮箱登录，虚拟用户发帖，举报，第三方广告，会员模块，即时通讯IM ，好友模块，投票，打赏，用户经验等级等丰富功能
+        </p>
+        <p>
+          <b>当前版本:</b> <span>开源版</span>
+        </p>
+        <p>
+          <b>项目官网:</b> <a href="https://net.linfeng.tech" target="_blank">https://net.linfeng.tech</a><br>
+          <b>公司官网:</b> <a href="https://www.linfengtech.cn" target="_blank">https://www.linfengtech.cn</a><br>
+          <b>微信公众号:</b> <span>林风科技</span>
+        </p>
+        <p>
+          <b>标准版后台演示:</b> <a href="https://dev.linfeng.tech" target="_blank">https://dev.linfeng.tech</a>
+        </p>
+        
+        <p>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTo('https://gitee.com/virus010101/linfeng-community')"
+            >开源下载</el-button
+          >
+          <el-button
+            size="mini"
+            icon="el-icon-s-home"
+            plain
+            @click="goTo('https://net.linfeng.tech')"
+            >访问项目官网</el-button
+          >
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTo('https://net.linfeng.tech/version/version.html')"
+            >功能清单</el-button
+          >
+          <el-button
+            type="warning"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTo('https://net.linfeng.tech/commercial.html')"
+            >商业版咨询</el-button
+          >
+        </p>
+      </el-col>
+
+      <el-col :sm="24" :lg="12" style="padding-left: 50px">
+        <el-row>
+          <el-col :span="12">
+            <h2>技术栈</h2>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6">
+            <h4>后端技术</h4>
+            <ul>
+              <li>SpringBoot</li>
+              <li>Shiro</li>
+              <li>JWT</li>
+              <li>MyBatisPlus</li>
+              <li>Quartz</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+          <el-col :span="6">
+            <h4>前端技术</h4>
+            <ul>
+              <li>Uniapp</li>
+              <li>Vue</li>
+              <li>Axios</li>
+              <li>Sass</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-divider />
   </div>
 </template>
 <script>
@@ -91,6 +182,9 @@ export default {
     };
   },
   methods: {
+    goTo(href) {
+      window.open(href, "_blank");
+    },
     statisticsOrder() {
       this.dataListLoading = true;
       this.$http({
@@ -111,6 +205,13 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+.acea-row{
+  display: flex;
+  flex-wrap: wrap;
+}
+.row-between-wrapper{
+  justify-content: space-between;
+}
 .ivu-mb {
   margin-bottom: 20px;
 }
